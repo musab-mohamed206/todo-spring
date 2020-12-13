886553a2a2111eac285b5bf1c77ffd0fb40bedca
NINJA <musab.mohamed206@gmail.com>
2020-12-13 10:57:13 +0300

    add delete funtion

--- src/main/java/com/test/todo/web/ProjectTaskController.java
+++ src/main/java/com/test/todo/web/ProjectTaskController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,5 +38,11 @@ public class ProjectTaskController {
     public ResponseEntity<?> getPTByID(@PathVariable Long pt_id) {
         ProjectTask projectTask = projectTaskService.findById(pt_id);
         return new ResponseEntity<ProjectTask>(projectTask , HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{pt_id}")
+    public ResponseEntity<?> deleteProjectTask(@PathVariable Long pt_id) {
+        projectTaskService.delete(pt_id);
+        return new ResponseEntity<String>("Project Task deleted" , HttpStatus.OK);
     }
 }
